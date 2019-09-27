6470c8f9e12b7665cc3943d11bd6b40cd1423d53
sourav-khunger <khungersourav30@gmail.com>
2019-09-27 18:57:29 +0530

    Event Fixed ! Video call room error removed! Landscape mode

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/myhexaville/androidwebrtc/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/myhexaville/androidwebrtc/R.java
@@ -25,6 +25,10 @@ public final class R {
     public static final int design_bottom_sheet_slide_out=0x7f01000d;
     public static final int design_snackbar_in=0x7f01000e;
     public static final int design_snackbar_out=0x7f01000f;
+    public static final int fade_in=0x7f010010;
+    public static final int fade_out=0x7f010011;
+    public static final int slide_down=0x7f010012;
+    public static final int slide_up=0x7f010013;
   }
   public static final class animator {
     public static final int design_appbar_state_list_animator=0x7f020000;
