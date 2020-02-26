b02aedb06e3445b64931b9f6e49a2fdc3e36a471
sourav-khunger <khungersourav30@gmail.com>
2020-02-26 11:04:06 +0530

    Remove duplicated directory

--- app/src/main/java/com/myhexaville/androidwebrtc/app_rtc/Services/AdminReceiver.java
+++
@@ -1,6 +0,0 @@
-package com.myhexaville.androidwebrtc.app_rtc.Services;
-
-import android.app.admin.DeviceAdminReceiver;
-
-public class AdminReceiver extends DeviceAdminReceiver {
-}
--- app/src/main/java/com/myhexaville/androidwebrtc/app_rtc/Utils/GenericFileProvider.java
+++
@@ -1,6 +0,0 @@
-package com.myhexaville.androidwebrtc.app_rtc.Utils;
-
-import android.support.v4.content.FileProvider;
-
-// class if server gets an update to store updated apk file
-public class GenericFileProvider extends FileProvider {}
